a7e182e07128183939bdb7e33d6c296d2a4fe8c1
ErickAndagoya <37913939+ErickAndagoya@users.noreply.github.com>
2018-03-30 09:07:19 -0500

    acelerometro

---
+++ acelerometro/src/acelerometro/Acelerometro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acelerometro;
+
+/**
+ *
+ * @author EQÚIPO
+ */
+public class Acelerometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmAcelerometro frmtraga = new FrmAcelerometro();
+        frmtraga.setVisible(true);
+    
+    }
+    
+}
